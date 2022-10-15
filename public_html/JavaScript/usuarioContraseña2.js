23afcd5bc23eba378e5f72e919a8e9819ff4b9ff
xdgtweb <xdgtweb@gmail.com>
2022-10-15 12:31:26 +0200

    x

--- public_html/JavaScript/usuarioContraseña2.js
+++ public_html/JavaScript/usuarioContraseña2.js
@@ -17,11 +17,13 @@ function orden()
     // Obtenemos las variables guardadas en items de localStorage
 
     // Si el usuario GUARDADO NO es correcto (aunque no tenga nada)
-    if (!(esUsuarioCorrecto(usuarioGuardado) && esContraseñaCorrecta(contraseñaGuardada)))
+    if ((esUsuarioCorrecto(usuarioGuardado) && esContraseñaCorrecta(contraseñaGuardada)))
     {
-
-    } else
+        alert("SI");
+    } 
+    else
     {
+        alert("NO");
         window.location.href = "https://xdgtweb.github.io/Rick_Y_Morty/public_html/Acceso.html";
     }
 }
